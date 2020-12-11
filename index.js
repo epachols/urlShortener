4fe4fbca2735f35d36aae649617bc9cce5bd4827
Capitol Heel <66080081+epachols@users.noreply.github.com>
2020-12-11 11:49:20 -0800

    set up post request for slug and nanoid, set to 7 for 65mil + options

--- index.js
+++ index.js
@@ -1,19 +1,17 @@
-const express = require ('express');
-const cors = require ('cors');
-const morgan = require('morgan');
-const helmet = require('helmet');
-const yup = require('yup');
-const nanoid = require(nanoid);
+const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
+const helmet = require("helmet");
+const yup = require("yup");
+const { nanoid } = require("nanoid");
 
 const app = express();
 
 app.use(helmet());
-app.use(morgan('tiny'));
+app.use(morgan("tiny"));
 app.use(cors());
 app.use(express.json());
-app.use(express.static('./public'))
-
-
+app.use(express.static("./public"));
 
 // app.get('/url/:id', (req,res) => {
 //     res.json({
@@ -27,21 +25,47 @@ app.use(express.static('./public'))
 //     })
 // });
 
-
 const schema = yup.object().shape({
-    slug: yup.string().trim().matches(/[\w\-]/i),
-    url:yup.string().trim().url().required(),
-})
-
-app.post('/url', (req,res) => {
-    res.json({
-    //    TODO: create a short url
-    })
+  slug: yup
+    .string()
+    .trim()
+    .matches(/[\w\-]/i),
+  url: yup.string().trim().url().required(),
 });
 
+app.post("/url", async (req, res, next) => {
+  let { slug, url } = req.body;
+  try {
+    await schema.validate({
+      slug,
+      url,
+    });
+    if (!slug) {
+      slug = nanoid(7);
+    }
+    slug = slug.toLowerCase();
+    res.json({
+      slug,
+      url,
+    });
+  } catch (error) {
+    next(error);
+  }
+});
 
+app.use((error, req, res, next) => {
+  if (error.status) {
+    res.status(error.status);
+  } else {
+    res.status(500);
+  }
+  res.json({
+    message: error.message,
+    stack: process.env.NODE_ENV === "production" ? "🥞" : error.stack,
+  });
+});
 
-const port = process.env.PORT || 1337
-app.listen(port, ()=> {
-    console.log(`listening at http://localhost:${port}`);
+const port = process.env.PORT || 1337;
+app.listen(port, () => {
+  console.log(`listening at http://localhost:${port}`);
 });
