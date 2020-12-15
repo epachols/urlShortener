aa3bdb19261e01a99699c54677b75d4bb1ad88e4
Capitol Heel <66080081+epachols@users.noreply.github.com>
2020-12-14 16:11:09 -0800

    attempting a change of MONGODB_URI from MONGODB_URL

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const monk = require("monk");
 
 // require("dotenv").config();
 
-const db = monk(process.env.MONGODB_URL);
+const db = monk(process.env.MONGODB_URI);
 const urls = db.get("urls");
 urls.createIndex({ url: 1, slug: 1 }, { unique: true });
 
